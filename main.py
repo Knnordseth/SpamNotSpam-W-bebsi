f60a15637a28fec7078afd30b1487af75bcdad68
Kristian <bollepusdodgebolle@gmail.com>
2023-03-14 09:55:31 +0100

    Update main.py
    fjerna tkinter, replaca med pygame, implementert code fra stackoverflow for text boksen, men mangler lagring av Text for å checke m spam filter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import json
-import threading
-import tkinter as tk
+#fjerna tkinter
+import pygame as pg #lagt til pygame
 
 #prøv å ordne loada inn json script stuff
 f = open('SPAMKEYWORDS.json')
@@ -36,25 +36,96 @@ def check_spam_gui():
 
     # GUI code goes here
     # Create the GUI window and input textbox
-    window = tk.Tk()
-    window.title("Spam Checker")
-    input_box = tk.Text(window, height=10, width=50)
-    input_box.insert(tk.END, "Paste your email here to check for spam")
-    input_box.pack()
- 
-     # Create a button to check for spam, lagt til threading, fordi systemet kanskje tror at programmet er uresponsivt og lukker det.
-    check_button = tk.Button(window, text="Check for spam", command=lambda: threading.Thread(target=check_spam).start())
-    check_button.pack()
-    
-    # Create a label to display the result
-    result_label = tk.Label(window, text="resultat")
-    result_label.pack()
-    
-    # Start the GUI event loop
-    window.mainloop()
+
+
+
+def main():
+    screen = pg.display.set_mode((640, 480))
+    font = pg.font.Font(None, 32)
+    clock = pg.time.Clock()
+    input_box = pg.Rect(100, 100, 140, 32)
+    color_inactive = pg.Color('lightskyblue3')
+    color_active = pg.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pg.KEYDOWN:
+                if active:
+                    if event.key == pg.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pg.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        screen.fill((251, 242, 199))
+
+        # Render the current text.
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pg.draw.rect(screen, color, input_box, 2)
+
+        pg.display.flip()
+        clock.tick(30)
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            mouse_pos = pg.mouse.get_pos()
+            if rectangle_rect.collidepoint(mouse_pos):
+                check_spam_gui()
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            mouse_pos = pg.mouse.get_pos()
+            if rectangle_rect.collidepoint(mouse_pos):
+                check_spam()
+
+    # Draw the rectangle onto the screen
+    screen.blit(rectangle_surface, rectangle_rect)
+
+    # Update the display
+    pg.display.update()
+
+
+if __name__ == '__main__':
+    pg.init()
+    main()
+    pg.quit()
+
 
 # Start the GUI for checking spam emails
 print("hello word, gui success!")
 load_spam_keywords()
+
+
 
 
